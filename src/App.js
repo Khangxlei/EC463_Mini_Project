40e3075feee0de3858c2b5465fc96c9ed304f7af
Marybel Boujaoude <91172956+marybelb@users.noreply.github.com>
2023-09-18 17:28:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+// Import the required components
 import Register from "./pages/Register"
 import Login from "./pages/Login"
 import Home from "./pages/Home"
-import "./style.scss"
+import "./style.scss" // Import styling file
 import {BrowserRouter,Routes,Route, Navigate,} from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 
 function App() {
-
+  // Get the currentUser from AuthContext
   const {currentUser} = useContext(AuthContext)
 
-  console.log(currentUser)
+  console.log(currentUser) // Log the currentUser
   
   const ProtectedRoute = ({children}) => {
+    // If currentUser does not exist, redirect user to login page
     if (!currentUser){
       return <Navigate to="/login"/>
     }
@@ -20,6 +22,7 @@ function App() {
     return children
   }
   return (
+    // Set up application routing
     <BrowserRouter>
       <Routes>
         <Route path="/">
