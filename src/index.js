04dbc5b9d3f00bca85b631343182ad862570d23b
Marybel Boujaoude <91172956+marybelb@users.noreply.github.com>
2023-09-19 16:11:21 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import { AuthContextProvider } from "./context/AuthContext";
-// import { ChatContextProvider } from "./context/ChatContext";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <AuthContextProvider>
-//     <ChatContextProvider>
-//       <React.StrictMode>
-//         <App />
-//       </React.StrictMode>
-//     </ChatContextProvider>
-//   </AuthContextProvider>
-// );
-
 // Import necessary components
 import React from 'react'
 import ReactDOM from 'react-dom/client'
