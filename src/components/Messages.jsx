3de4b49e27bcd975fcbda001e5d45b512c849655
Marybel Boujaoude <91172956+marybelb@users.noreply.github.com>
2023-09-18 22:30:53 -0400

    Update Messages.jsx

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,17 +1,18 @@
+// Import necessary components
 import { doc, onSnapshot } from "firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
 import { ChatContext } from "../context/ChatContext";
 import { db } from "../firebase";
 import Message from "./Message";
 
-
+// Message components 
 const Messages = () => {
-  const [messages, setMessages] = useState([]);
-  const { data } = useContext(ChatContext);
+  const [messages, setMessages] = useState([]); // State for storing messages
+  const { data } = useContext(ChatContext); // Accessing data from ChatContext
 
   useEffect(() => {
     const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
-      doc.exists() && setMessages(doc.data().messages);
+      doc.exists() && setMessages(doc.data().messages); // Updates messages when data changes
     });
 
     return () => {
@@ -19,7 +20,7 @@ const Messages = () => {
     };
   }, [data.chatId]);
 
-  console.log(messages)
+  console.log(messages) // Log messages to console 
 
   return (
     <div className="messages">
