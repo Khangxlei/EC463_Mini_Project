89eaa235fe11f1f2f8bd0ce2e247ddf63dfb12de
Marybel Boujaoude <91172956+marybelb@users.noreply.github.com>
2023-09-19 16:10:44 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,30 +1,8 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getStorage } from "firebase/storage";
-// import { getFirestore } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-//   authDomain: "chat-ab746.firebaseapp.com",
-//   projectId: "chat-ab746",
-//   storageBucket: "chat-ab746.appspot.com",
-//   messagingSenderId: "901216368405",
-//   appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
-// export const auth = getAuth();
-// export const storage = getStorage();
-// export const db = getFirestore()
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 import { getStorage, ref } from "firebase/storage";
-
-
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
